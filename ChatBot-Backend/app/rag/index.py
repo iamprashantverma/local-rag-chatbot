3050291272f58fe18b7844328b004e80215ab5e6
iamprashantverma <prashant2k26@gmail.com>
2026-02-18 13:16:47 +0530

    feat :Build complete RAG workflow with LangChain and Chroma

---
+++ ChatBot-Backend/app/rag/index.py
@@ -0,0 +1,20 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+from app.rag.loader import load_docs
+
+def build_index():
+    docs = load_docs()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    chunks = splitter.split_documents(docs)
+
+    embeddings = HuggingFaceEmbeddings()
+
+    db = Chroma.from_documents(chunks,embeddings,persist_directory="vectordb")
+
+    db.persist()
+
+if __name__ == "__main__":
+    build_index()
