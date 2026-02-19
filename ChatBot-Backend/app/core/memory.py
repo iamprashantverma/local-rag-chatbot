145f7b49ff2001207eef643f34742d4cffbb211b
iamprashantverma <prashant2k26@gmail.com>
2026-02-19 13:22:12 +0530

    fix : Context size is fixed & only sending last 15 msgs

--- ChatBot-Backend/app/core/memory.py
+++ ChatBot-Backend/app/core/memory.py
@@ -5,4 +5,8 @@ store = {}
 def get_session_history(session_id: str):
     if session_id not in store:
         store[session_id] = ChatMessageHistory()
-    return store[session_id]
+
+    history = store[session_id]
+    history.messages = history.messages[-15:]  
+    return history
+
