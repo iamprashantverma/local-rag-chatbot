e580eb2e937d4f6dd9ce7b5d7bfa6facd56d143c
iamprashantverma <prashant2k26@gmail.com>
2026-02-21 11:57:58 +0530

    feat:docs_router added in main router

--- ChatBot-Backend/app/api/router.py
+++ ChatBot-Backend/app/api/router.py
@@ -3,8 +3,9 @@ from app.api.chat_routes import router as chat_router
 from app.api.health_routes import router as health_router
 from app.api.auth_routes import router as auth_router
 from app.api.deps import get_current_user
-
+from app.api.docs_route import router as docs_router
 api_router = APIRouter()
 api_router.include_router(health_router)
 api_router.include_router(auth_router)
+api_router.include_router(docs_router,dependencies=[Depends(get_current_user)])
 api_router.include_router(chat_router,dependencies=[Depends(get_current_user)])
