e0a0ef463c6a22ea1dc7f1308d67aea9ea42fc0a
iamprashantverma <prashant2k26@gmail.com>
2026-02-24 11:18:00 +0530

    feat : stablished the relationship between user and chat table

--- ChatBot-Backend/app/models/chat.py
+++ ChatBot-Backend/app/models/chat.py
@@ -8,5 +8,5 @@ class Chat(Base):
     id = Column(Integer, primary_key=True, index=True)
     role = Column(String(20))
     content = Column(String(2000))
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_email = Column(String(50), ForeignKey("users.email"), nullable=False)  
     user = relationship("User", back_populates="chats")
--- ChatBot-Backend/app/models/user.py
+++ ChatBot-Backend/app/models/user.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.db.base import Base
 
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)  
+    name = Column(String(100), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
+    chats = relationship("Chat", back_populates="user", cascade="all, delete-orphan")
