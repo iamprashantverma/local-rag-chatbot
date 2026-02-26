8c4183a16b9a0e0b0cc801dbe883e7353d96ad47
iamprashantverma <prashant2k26@gmail.com>
2026-02-26 10:57:46 +0530

    feat(api): add global axios response interceptor with toast error handling

--- chatbot-frontend/src/services/api/axios.js
+++ chatbot-frontend/src/services/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
 
 api.interceptors.request.use(
   (config) => {
@@ -16,6 +17,20 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (!error.response) {
+      toast.error("Server is unavailable. Please try again later.");
+    } else {
+      const message = error.response.data?.detail ||error.response.data?.message || "Something went wrong.";
+      toast.error(message);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
