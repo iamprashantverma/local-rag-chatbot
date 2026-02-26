f9259b2a575eae52976e0d61ee1ce7e41fe57899
iamprashantverma <prashant2k26@gmail.com>
2026-02-26 10:37:00 +0530

    feat: add global exception handlers for external API, validation, and server errors

--- ChatBot-Backend/app/main.py
+++ ChatBot-Backend/app/main.py
@@ -3,6 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 from app.db.base import Base
 from app.db.session import engine 
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import requests
 
 app = FastAPI()
 origins = ["*"]
@@ -18,3 +21,29 @@ app.add_middleware(
 Base.metadata.create_all(bind=engine)
 
 app.include_router(api_router)
+
+@app.exception_handler(requests.exceptions.RequestException)
+async def external_api_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=503,
+        content={"detail": "External service unavailable. Please try again later."},
+    )
+
+
+# Handle validation errors like unknown coin
+@app.exception_handler(ValueError)
+async def value_error_handler(request: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": str(exc)},
+    )
+
+
+# Catch-all handler (VERY IMPORTANT)
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal server error."},
+    )
+
